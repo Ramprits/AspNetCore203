4281c4d86a172d4f13f4da51f7cce73b41f0e906
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-21 17:30:15 +0530

    Speaker Vm added

---
+++ ViewModels/SpeakerVm.cs
@@ -0,0 +1,12 @@
+namespace AspNet_core_203.ViewModels {
+    public class SpeakerVm {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string TwitterName { get; set; }
+        public string GitHubName { get; set; }
+        public string Bio { get; set; }
+        public string HeadShotUrl { get; set; }
+    }
+}
